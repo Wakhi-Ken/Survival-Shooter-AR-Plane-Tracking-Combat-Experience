eb6105a6089c9caf43fcce5f72995c5cb55b2c05
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-06-16 15:05:08 +0200

    MenuButtons working

--- Assets/Scripts/MenuControls.cs
+++ Assets/Scripts/MenuControls.cs
@@ -1,16 +1,80 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Canvas")]
+    public GameObject pauseCanvas;
+
+    private bool isPaused = false;
+
+    private void Start()
     {
-        
+        Time.timeScale = 1f;
+
+        if (pauseCanvas != null)
+            pauseCanvas.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+  
+    // STAGE LOADING
+   
+
+    public void LoadStage1()
     {
-        
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void LoadStage2()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Stage2");
+    }
+
+    // PAUSE SYSTEM
+   
+
+    public void PauseGame()
+    {
+        isPaused = true;
+
+        Time.timeScale = 0f;
+
+        if (pauseCanvas != null)
+            pauseCanvas.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+
+        Time.timeScale = 1f;
+
+        if (pauseCanvas != null)
+            pauseCanvas.SetActive(false);
+    }
+
+    
+    // MAIN MENU
+    
+
+    public void ReturnToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Start-Menu");
+    }
+
+    
+    // QUIT GAME
+  
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
