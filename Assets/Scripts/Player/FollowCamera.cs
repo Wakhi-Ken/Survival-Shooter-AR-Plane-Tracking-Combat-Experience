1f79db0dac04a6ea250163cbbb10eb661e190b3e
Wakhile Dlamini <w.dlamini@alustudent.com>
2026-06-20 10:40:53 +0200

    enemies face player when spawned

--- Assets/Scripts/Player/FollowCamera.cs
+++ Assets/Scripts/Player/FollowCamera.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    public Transform arCamera;
+    private Transform arCamera;
+
+    void Start()
+    {
+        if (Camera.main != null)
+        {
+            arCamera = Camera.main.transform;
+        }
+    }
 
     void Update()
     {
@@ -11,7 +19,7 @@ public class FollowCamera : MonoBehaviour
         Vector3 direction =
             arCamera.position - transform.position;
 
-        direction.y = 0f; // keep character upright
+        direction.y = 0f; // keep upright
 
         if (direction.sqrMagnitude > 0.01f)
         {
